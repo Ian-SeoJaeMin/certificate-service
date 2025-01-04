0e6b7194d8bdb070fbfd15b1a6e2d1387b5d739f
Ian-SeoJaeMin <animomin@daum.net>
2025-01-05 00:15:53 +0900

    feat: jwt 토큰 발행 기능 구현 중

--- src/certificate/certificate.service.ts
+++ src/certificate/certificate.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { TokenUserDto } from './dto/token-user.dto';
 
 @Injectable()
-export class CertificateService {}
+export class CertificateService {
+    constructor() {}
+
+    issueToken(tokenUserDto: TokenUserDto) {
+        const { userType } = tokenUserDto;
+    }
+}
